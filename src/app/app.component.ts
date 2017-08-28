c31e727b847cf7e36d9310ee8d0091806d45ea03
dimitartalanov <d_talanov@yahoo.com>
2017-08-28 17:24:18 +0300

    isAuth in app.cpomponent get-loggedUserId, logout-remove-loggedUserId

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './shared/services';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
 export class AppComponent {
+  private isAuthenticated: boolean;
+  constructor(private authService: AuthService) {
+    authService.authState.subscribe((state: AngularFireAuthModule) => {
+      this.isAuthenticated = state !== null;
+    });
+  }
+  onSignOutClick() {
+    this.authService.signOut();
+  }
 }
